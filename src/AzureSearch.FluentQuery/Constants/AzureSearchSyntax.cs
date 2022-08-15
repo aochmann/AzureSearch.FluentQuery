51cc5827b77d7137fcd0266040ef0a6ff034269a
Adrian Ochmann <adrian.ochmann@outlook.com>
2022-08-15 21:41:22 +0200

    feat: added constants

---
+++ src/AzureSearch.FluentQuery/Constants/AzureSearchSyntax.cs
@@ -0,0 +1,16 @@
+namespace AzureSearch.FluentQuery.Constants;
+
+public static class AzureSearchSyntax
+{
+    public const string Equal = "eq";
+    public const string NotEqual = "ne";
+    public const string And = "and";
+    public const string Or = "or";
+    public const string GreaterThan = "gt";
+    public const string GreaterThanOrEqual = "ge";
+    public const string LessThan = "lt";
+    public const string LessThanOrEqual = "le";
+    public const string Not = "not";
+    public const string Any = "any";
+    public const string All = "all";
+}
---
+++ src/AzureSearch.FluentQuery/Constants/Separators.cs
@@ -0,0 +1,12 @@
+namespace AzureSearch.FluentQuery.Constants;
+
+internal static class Separators
+{
+    public const string Comma = ",";
+    public const string Space = " ";
+    public const string OpenParenthesis = "(";
+    public const string CloseParenthesis = ")";
+    public const string ArrowFunction = "=>";
+    public const string Slash = "/";
+
+}
